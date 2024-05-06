a1ba962c75ca0024b901bbdd076cba2e558726aa
Aman Ahamed <amanahamed04gmail.com>
2024-05-06 13:39:40 +0530

    React Native Color Picker App

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
+const placeHolder = require("./assets/images/background-image.png");
+// rnfes
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
   return (
+    // view is like a div element
+    // use style={{'color':'#....'}} or style={styles.className}
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        {/* it is a component with 2 props */}
+        <ImageViewer
+          placeholderImageSource={placeHolder}
+          selectedImage={selectedImage}
+        />
+      </View>
+      <View style={styles.footerContainer}>
+        {/* it is a component with 3 props */}
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// styles object --> use it inside retun with styles.(name)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
